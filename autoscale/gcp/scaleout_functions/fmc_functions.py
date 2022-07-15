a439cac26ab61fdb67fba19d56763fa861e0220d
Akshay Nadgir <63632514+aknadgir@users.noreply.github.com>
2022-07-15 13:34:48 +0530

    Addressed Some GCP Autoscale Changes

--- autoscale/gcp/scaleout_functions/fmc_functions.py
+++ autoscale/gcp/scaleout_functions/fmc_functions.py
@@ -219,7 +219,6 @@ class FirepowerManagementCenter:
           Returns:    REST post response
           Raises:
           """
-          lic_caps = os.getenv('LICENSE_CAPS')
           lic_caps = self.lic_caps.split(",")
           print("Registering: "+ name)
           api_path = "/api/fmc_config/v1/domain/e276abec-e0f2-11e3-8169-6d9ed49b625f/devices/devicerecords"
