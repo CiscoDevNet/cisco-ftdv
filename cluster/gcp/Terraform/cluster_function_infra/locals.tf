248f0a35f17c6471bfd6584d508f1278fc3ec6dc
Nanditha <nandis@cisco.com>
2024-08-20 12:01:33 +0530

    Remove GCP cluster terraform scripts

--- cluster/gcp/Terraform/cluster_function_infra/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  filter_rule = "(resource.type=\"gce_instance_group_manager\" AND resource.labels.instance_group_manager_name = \"${var.resourceNamePrefix}-ftdv-instance-group\" AND protoPayload.methodName = \"v1.compute.regionInstanceGroupManagers.insert\" AND operation.last = true)  OR (resource.type = \"cloud_function\" AND resource.labels.function_name = \"${var.resourceNamePrefix}-ftdv-cluster-action\" AND textPayload:\"Reattempt\")"
-  cluster_name = "${var.resourceNamePrefix}-cluster"
-}
-
